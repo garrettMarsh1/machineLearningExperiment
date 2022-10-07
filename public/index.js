e1203fd24fbc03ba89a73b9a6be6ebc915b5c131
Garrett Marsh <104183243+garrettMarsh1@users.noreply.github.com>
2022-10-07 01:58:18 -0400

    Data now saves between data and local storage
    There is a problem with saving, needs to be optimized

--- public/index.js
+++ public/index.js
@@ -1,21 +1,21 @@
 
 
 
-const carCanvas=document.getElementById("carCanvas");
-carCanvas.width=200;
-const networkCanvas=document.getElementById("networkCanvas");
-networkCanvas.width=300;
-carCanvas.width=200;
-const carCtx=carCanvas.getContext("2d");
-const networkCtx=networkCanvas.getContext("2d");
-const road=new Road(carCanvas.width/2,carCanvas.width*0.9);
+const carCanvas = document.getElementById("carCanvas");
+carCanvas.width = 200;
+const networkCanvas = document.getElementById("networkCanvas");
+networkCanvas.width = 300;
+carCanvas.width = 200;
+const carCtx = carCanvas.getContext("2d");
+const networkCtx = networkCanvas.getContext("2d");
+const road = new Road(carCanvas.width/2,carCanvas.width*0.9);
 
 
 
 
 
 //N = number of cars generated
-const N = 750;
+const N = 10;
 // N2 = number of traffic generated
 const N2 = 50;
 
@@ -35,34 +35,79 @@ let worstPath = cars[0];
 // let worstTrafficPath = traffic[0];
 
 //saving best runs for car
-if(localStorage.getItem("bestRuns")){
-    for(let i = 0; i < cars.length; i++){
-        cars[i].brain = JSON.parse
-        (localStorage.getItem("bestRuns"));
-        if(i!=0){
-            NeuralNetwork.mutate(cars[i].brain, .1);
-        }
-       
-    }
-}
+(async function () {
+    // body of the function
+    if (localStorage.getItem("bestRuns")) {
+    
+        const request = await fetch("/pull");
+        const response = await request.json();
+    
+        if (response.message) console.log(response);
+        console.log(JSON.parse(localStorage.getItem("bestRuns")))
 
-if(localStorage.getItem("bestTrafficRuns")){
-    for(let i = 0; i < traffic.length; i++){
-        traffic[i].brain = JSON.parse
-        (localStorage.getItem("bestTrafficRuns"));
-        if(i!=0){
-            NeuralNetwork.mutate(traffic[i].brain, .01);
+        let syncData = response.data.map( data => {
+            return data.levels;
+        })
+
+
+        syncData = syncData.flat(6);
+
+        console.log(syncData);
+    
+        for(let i = 0; i < cars.length; i++){
+    
+    
+            cars[i].brain = (syncData.length != 0) ? { levels: syncData } : JSON.parse(localStorage.getItem("bestRuns"));
+    
+            if(i!= 0){
+                NeuralNetwork.mutate(cars[i].brain, .1);
+            }
+           
         }
-       
+    
     }
-}
+    
+    if(localStorage.getItem("bestTrafficRuns")){
+        for(let i = 0; i < traffic.length; i++){
+            traffic[i].brain = JSON.parse
+            (localStorage.getItem("bestTrafficRuns"));
+            if(i!= 0){
+                NeuralNetwork.mutate(traffic[i].brain, .01);
+            }
+           
+        }
+    }
+    
+}());
 
 animate();
 
+
 //saving and discard best/worst traffic runs from storage
-function saveBestCar() {
+async function saveBestCar() {
     localStorage.setItem("bestRuns", 
     JSON.stringify(bestPath.brain));
+
+
+    const options = {
+        method: 'POST',
+        credentials: 'same-origin',
+        headers: {
+            'Content-Type': 'application/json'
+
+        },
+        body: JSON.stringify(bestPath.brain)
+    }
+
+    console.log('Best Car:', bestPath);
+
+    const request = await fetch("/sync", options);
+    const response = await request.json();
+
+    console.log(response);
+
+    if (response.ok) console.log("successfully saved data to server");
+    if (response.error) console.log("Roh oh raggie");
 }
 
 function saveBestTraffic() {
@@ -93,8 +138,8 @@ function discardBestCars(){
  * @returns 
  */
 function generateCars(N){
-    let cars=[];
-    for(let i =1;i<=N;i++){
+    let cars = [];
+    for(let i = 1; i <= N;i++){
         cars.push(new Car(road.getLaneCenter(1),100,30,50,"AI"));
         
         
@@ -110,9 +155,9 @@ function generateCars(N){
  * @returns 
  */
  function generateTraffic(N2){
-    let traffic=[];
+    let traffic = [];
     
-    for(let i =1;i<=N2;i++){
+    for(let i = 1;i <= N2; i++){
         
         let trafficItem = new Traffic(road.getLaneCenter(random(0,3)),
         random(-20000, 0),30,50,"AI", random(3,5));
@@ -128,10 +173,10 @@ function generateCars(N){
 function animate(time){
 
     
-    for(let i=0; i<traffic.length; i++){
+    for(let i = 0; i<traffic.length; i++){
         traffic[i].update(road.borders, [],traffic, cars);
     }
-    for(let i=0; i<cars.length; i++){
+    for(let i = 0; i<cars.length; i++){
         cars[i].update(road.borders, traffic, cars);
     }
 
@@ -139,36 +184,37 @@ function animate(time){
 
     // logic for the best car path
     bestPath = cars.find(
-        c=>c.y==Math.min(
-            ...cars.map(c=>c.y),
+        c => c.y == Math.min(
+            ...cars.map(c => c.y),
         ));
 
     bestTrafficPath = traffic.find(
-        c=>c.y==Math.min(
-            ...traffic.map(c=>c.y),
+        c => c.y == Math.min(
+            ...traffic.map(c => c.y),
         ));
 
 
     // logic for the worst car path
     worstPath = cars.find(
-        c=>c.y==Math.max(
-            ...cars.map(c=>c.y)
+        c => c.y == Math.max(
+            ...cars.map(c => c.y)
 
         ));
     // logic for the worst car path
     worstTrafficPath = traffic.find(
-        c=>c.y==Math.max(
-            ...traffic.map(c=>c.y)
+        c => c.y == Math.max(
+            ...traffic.map(c => c.y)
 
         ));
    
     //if %99 cars are damaged, save best path, discard worst, then reload page.
-    if(cars.filter(c=>c.damaged).length>=cars.length*0.95){
+    if (cars.filter(c => c.damaged).length >=  cars.length*0.35) {
         
-        saveBestCar();
-        saveBestTraffic();
         discardWorstTraffic();
         discardWorstCars();
+        discardBestCars();
+        saveBestCar();
+        saveBestTraffic(); 
         history.go(0);
     }
 
@@ -190,7 +236,7 @@ function animate(time){
     road.draw(carCtx);
 
     // drawing traffic
-    for(let i =0; i<traffic.length; i++){
+    for(let i  = 0; i<traffic.length; i++){
         traffic[i].draw(carCtx, "green");
     }
   
@@ -198,7 +244,7 @@ function animate(time){
     carCtx.globalAlpha = 0.2;
 
     // drawing cars
-    for(let i=0; i<cars.length; i++){
+    for(let i = 0; i<cars.length; i++){
         cars[i].draw(carCtx, "orange");
     }
     // opacity of the best car in current run
